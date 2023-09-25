838be3fe819c359185a9ce9d8ef25d82d4b5010e
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-09-25 10:12:19 -0700

    Odo Peter Ebere - task checks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""python script that uses JSONPlaceholder API to get information about employee """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    target_url = 'https://jsonplaceholder.typicode.com/'
+
+    user = '{}users/{}'.format(target_url, sys.argv[1])
+    res = requests.get(user)
+    json_obj = res.json()
+    print("Employee {} is done with tasks".format(json_obj.get('name')), end="")
+
+    todos = '{}todos?userId={}'.format(target_url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            l_task.append(task)
+
+            print("({}/{}):".format(len(l_task), len(tasks)))
+            for task in l_task:
+                print("\t {}".format(task.get("title")))
