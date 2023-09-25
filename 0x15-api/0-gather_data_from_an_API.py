c74fe51aca6c63baee1c5772ead45cc9ba663d8c
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-09-25 10:19:40 -0700

    Odo Peter Ebere - fixed pystyles

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""python script that uses JSONPlaceholder API to get information about employee """
+"""py script that uses JSONPlaceholder API to get info about employee """
 import requests
 import sys
 
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     user = '{}users/{}'.format(target_url, sys.argv[1])
     res = requests.get(user)
     json_obj = res.json()
-    print("Employee {} is done with tasks".format(json_obj.get('name')), end="")
+    print("Employee {} is done with tasks".format(
+        json_obj.get('name')), end="")
 
     todos = '{}todos?userId={}'.format(target_url, sys.argv[1])
     res = requests.get(todos)
