f146fc406e643cf70cec5b80d3adca0ef4161e47
Odo-Peter <odopeter33@gmail.com>
2023-10-12 11:02:45 -0700

    advance api task

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the top 10
+posts for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    try:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        req = requests.get(
+            url, allow_redirects=False, headers={
+                'User-Agent': 'wg1987'},
+            params={'limit': 10})
+
+        children = req.json().get('data').get('children')
+
+        for child in children:
+            print(child.get('data').get('title'))
+    except ConnectionError:
+        print(None)
